2b35fd21c7050212b3a85318c8259c2b555256fe
Mohamed Abo-ghazalla <45886535+Abo-ghazalla@users.noreply.github.com>
2024-03-20 05:23:45 +0200

    create abstract RemoteDataSource

---
+++ lib/core/data_sources/remote_data_source.dart
@@ -0,0 +1,15 @@
+import 'package:nxt_assessment/core/model/app_base_response.dart';
+import 'package:nxt_assessment/core/model/base_model.dart';
+import 'package:nxt_assessment/core/model/to_json_convertible.dart';
+
+abstract class RemoteDataSource {
+  Future<AppBaseResponse<T>> get<T>({
+    required String path,
+    ToJsonConvertible? query,
+    BaseModel? model,
+  });
+
+  void setHeaderToken(String token);
+
+  void resetHeaderToken();
+}
